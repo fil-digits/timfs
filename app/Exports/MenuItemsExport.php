3056d8b719603ac85cc927a0a9342786d115605b
Mike Rodelas <mikerodelas@digits.ph>
2022-12-14 20:31:00 +0800

    export menu items adjustments

--- app/Exports/MenuItemsExport.php
+++ app/Exports/MenuItemsExport.php
@@ -2,7 +2,11 @@
 
 namespace App\Exports;
 
+use App\MenuChoiceGroup;
 use App\MenuItem;
+use App\MenuOldCodeMaster;
+use App\MenuPriceMaster;
+use App\MenuSegmentation;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -15,64 +19,86 @@ class MenuItemsExport implements FromQuery, WithHeadings, WithMapping
     use Exportable;
 
     public function headings():array{
-        $segmentations =  DB::table('menu_segmentations')->where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
-		$ingredients = DB::table('menu_ingredients')->where('status','ACTIVE')->orderBy('menu_ingredient_description','ASC')->get();
+        $segmentations =  MenuSegmentation::where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
+		$old_item_codes = MenuOldCodeMaster::where('status','ACTIVE')->orderBy('menu_old_code_column_description','ASC')->get();
+        $prices = MenuPriceMaster::where('status','ACTIVE')->orderBy('menu_price_column_description','ASC')->get();
+        $group_choices = MenuChoiceGroup::where('status','ACTIVE')->orderBy('menu_choice_group_column_description','ASC')->get();
+
+        $header = ['MENU CODE'];
+			
+        foreach($old_item_codes as $old_codes){
+            array_push($header,$old_codes->menu_old_code_column_description);
+        }
+
+        array_push($header,'POS OLD DESCRIPTION');
+        array_push($header,'MENU DESCRIPTION');
+        array_push($header,'PRODUCT TYPE');
+
+        foreach($group_choices as $choice){
+            array_push($header,$choice->menu_choice_group_column_description);
+            array_push($header,$choice->menu_choice_group_column_description.' SKU');
+        }
         
-        $header = [
-            'MENU CODE',
-            'MENU DESCRIPTION',
-            'PRODUCT TYPE',
-            'TRANSACTION TYPE',
-            'MENU TYPE',
-            'CATEGORY',
-            'PRICE',
-            'ORIGINAL CONCEPT',
-            'STATUS',
-            'APPROVED DATE',
-            'AVAILABLE CONCEPTS'
-        ];
+        array_push($header,'MENU TYPE');
+        array_push($header,'MAIN CATEGORY');
+        array_push($header,'SUB CATEGORY');
+
+        foreach($prices as $price){
+            array_push($header,$price->menu_price_column_description);
+        }
+        
+        array_push($header,'ORIGINAL CONCEPT');
+        array_push($header,'STATUS');
 
         foreach($segmentations as $segment){
             array_push($header,$segment->menu_segment_column_description);
         }
-        foreach($ingredients as $ingredient){
-            array_push($header,'INGREDIENT CODE '.$ingredient->menu_ingredient_description);
-            array_push($header,'INGREDIENT NAME '.$ingredient->menu_ingredient_description);
-            array_push($header,'INGREDIENT QTY '.$ingredient->menu_ingredient_description);
-        }
+
         return $header;
     } 
 
     public function map($data_menu_item): array {
-        $segmentations =  DB::table('menu_segmentations')->where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
-		$ingredients = DB::table('menu_ingredients')->where('status','ACTIVE')->orderBy('menu_ingredient_description','ASC')->get();
+        $segmentations =  MenuSegmentation::where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
+        $old_item_codes = MenuOldCodeMaster::where('status','ACTIVE')->orderBy('menu_old_code_column_description','ASC')->get();
+        $prices = MenuPriceMaster::where('status','ACTIVE')->orderBy('menu_price_column_description','ASC')->get();
+        $group_choices = MenuChoiceGroup::where('status','ACTIVE')->orderBy('menu_choice_group_column_description','ASC')->get();
         
         $data_items = [
-            $data_menu_item->tasteless_menu_code,
-            $data_menu_item->menu_item_description,
-            $data_menu_item->menu_product_type_description,
-            $data_menu_item->menu_transaction_type_description,
-            $data_menu_item->menu_type_description,
-            $data_menu_item->category_description,
-            $data_menu_item->menu_selling_price,
-            $data_menu_item->original_concept,
-            $data_menu_item->menu_item_status,
-            $data_menu_item->approved_at,
-            $data_menu_item->available_concepts
+            $data_menu_item->tasteless_menu_code
         ];
+
+        foreach($old_item_codes as $old_code){
+            $item_code = $old_code->menu_old_code_column_name;
+            array_push($data_items, $data_menu_item->$item_code);
+        }
+
+        array_push($data_items, $data_menu_item->pos_old_item_description);
+        array_push($data_items, $data_menu_item->menu_item_description);
+        array_push($data_items, $data_menu_item->menu_product_type_description);
+
+        foreach($group_choices as $group_choice){
+            $group = 'choices_'.$group_choice->menu_choice_group_column_name;
+            $groupSku = 'choices_sku'.$group_choice->menu_choice_group_column_name;
+
+            array_push($data_items, $data_menu_item->$group);
+            array_push($data_items, $data_menu_item->$groupSku);
+        }
+        
+        array_push($data_items, $data_menu_item->menu_type_description);
+        array_push($data_items, $data_menu_item->category_description);
+        array_push($data_items, $data_menu_item->subcategory_description);
+
+        foreach($prices as $price){
+            $item_price = $price->menu_price_column_name;
+            array_push($data_items, $data_menu_item->$item_price);
+        }
+
+        array_push($data_items, $data_menu_item->original_concept);
+        array_push($data_items, $data_menu_item->menu_item_status);
         
         foreach($segmentations as $segment){
             $seg = $segment->menu_segment_column_name;
             array_push($data_items, $data_menu_item->$seg);
-        }
-        foreach($ingredients as $ingredient){
-            $ing_code = 'ingredient_code_'.$ingredient->menu_ingredient_description;
-            $ing_name = 'ingredient_name_'.$ingredient->menu_ingredient_description;
-            $ing_qty = 'ingredient_qty_'.$ingredient->menu_ingredient_description;
-
-            array_push($data_items,$data_menu_item->$ing_code);
-            array_push($data_items,$data_menu_item->$ing_name);
-            array_push($data_items,$data_menu_item->$ing_qty);
         }
 
         return $data_items;
@@ -80,36 +106,45 @@ class MenuItemsExport implements FromQuery, WithHeadings, WithMapping
 
     public function query()
     {
-        $segmentations =  DB::table('menu_segmentations')->where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
-		$ingredients = DB::table('menu_ingredients')->where('status','ACTIVE')->orderBy('menu_ingredient_description','ASC')->get();
+        $segmentations =  MenuSegmentation::where('status','ACTIVE')->orderBy('menu_segment_column_description','ASC')->get();
+		$old_item_codes = MenuOldCodeMaster::where('status','ACTIVE')->orderBy('menu_old_code_column_description','ASC')->get();
+        $prices = MenuPriceMaster::where('status','ACTIVE')->orderBy('menu_price_column_description','ASC')->get();
+        $group_choices = MenuChoiceGroup::where('status','ACTIVE')->orderBy('menu_choice_group_column_description','ASC')->get();
 
         $menu_items = MenuItem::query()
         ->whereNotNull('tasteless_menu_code')
         ->leftJoin('menu_product_types','menu_items.menu_product_types_id','=','menu_product_types.id')
-        ->leftJoin('menu_transaction_types','menu_items.menu_transaction_types_id','=','menu_transaction_types.id')
         ->leftJoin('menu_types','menu_items.menu_types_id','=','menu_types.id')
         ->leftJoin('menu_categories','menu_items.menu_categories_id','=','menu_categories.id')
+        ->leftJoin('menu_subcategories','menu_items.menu_subcategories_id','=','menu_subcategories.id')
         ->select(
             'menu_items.tasteless_menu_code',
+            'menu_items.pos_old_item_description',
             'menu_items.menu_item_description',
             'menu_product_types.menu_product_type_description',
-            'menu_transaction_types.menu_transaction_type_description',
             'menu_types.menu_type_description',
             'menu_categories.category_description',
-            'menu_items.menu_selling_price',
+            'menu_subcategories.subcategory_description',
             'menu_items.original_concept',
             'menu_items.status as menu_item_status',
             'menu_items.approved_at',
             'menu_items.available_concepts');
             
+        foreach($old_item_codes as $old_codes){
+            $menu_items->addSelect('menu_items.'.$old_codes->menu_old_code_column_name);
+        }
+
         foreach($segmentations as $segment){
             $menu_items->addSelect('menu_items.'.$segment->menu_segment_column_name);
         }
 
-        foreach($ingredients as $ingredient){
-            $menu_items->addSelect('menu_items.ingredient_code_'.$ingredient->menu_ingredient_description);
-            $menu_items->addSelect('menu_items.ingredient_name_'.$ingredient->menu_ingredient_description);
-            $menu_items->addSelect('menu_items.ingredient_qty_'.$ingredient->menu_ingredient_description);
+        foreach($group_choices as $choice){
+            $menu_items->addSelect('menu_items.choices_'.$choice->menu_choice_group_column_name);
+            $menu_items->addSelect('menu_items.choices_sku'.$choice->menu_choice_group_column_name);
+        }
+
+        foreach($prices as $price){
+            $menu_items->addSelect('menu_items.'.$price->menu_price_column_name);
         }
 
         if (request()->has('filter_column')) {
@@ -171,7 +206,6 @@ class MenuItemsExport implements FromQuery, WithHeadings, WithMapping
                 }
             }
         }
-
 
         return $menu_items;
     }
