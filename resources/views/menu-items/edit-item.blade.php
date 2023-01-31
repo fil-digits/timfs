bab578f5e4f2aea23bb1a711c76382cb31597e7f
Fillinor <fillinorgunio@digits.ph>
2023-01-31 09:03:42 +0800

    preventing to submit the form when pressing enter in input fields

--- resources/views/menu-items/edit-item.blade.php
+++ resources/views/menu-items/edit-item.blade.php
@@ -233,6 +233,13 @@
                     }
                 });
             });
+
+            $(window).keydown(function(event){
+                if(event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+                }
+            });
         }
 
         $(document).on('click', '.list-item', function(event) { 
