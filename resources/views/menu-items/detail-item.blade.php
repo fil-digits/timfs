158f0791292c64ccc1f54ebf46c8e2fde67bc601
Fillinor <fillinorgunio@digits.ph>
2023-02-09 09:48:22 +0800

    only showing the primary / selected ingredients on view details page

--- resources/views/menu-items/detail-item.blade.php
+++ resources/views/menu-items/detail-item.blade.php
@@ -45,10 +45,8 @@
                 </tr>
             </tbody>
         </table>
-
-        @if(!count($ingredients))
-            <h4 style="color: gray; text-align: center; font-style: italic;"> <i class="fa fa-spoon"></i> No ingredients to show...</h4>
-        @else
+            <h4 class="no-ingredient-warning" style="color: gray; text-align: center; font-style: italic; display: none"> <i class="fa fa-spoon"></i> No ingredients to show...</h4>
+        <div class="with-ingredient" style="display: none;">
             <h4 style="font-weight: 600; text-align: center;">Ingredients List</h4>
             <div class="box-body table-responsive no-padding">
                 <table class="table table-striped table-bordered">
@@ -62,8 +60,8 @@
                         </tr>
                     </thead>
         
-                    <tbody>
-                        @foreach ($ingredients as $ingredient)
+                    <tbody class="ingredient-tbody">
+                        {{-- @foreach ($ingredients as $ingredient)
                             <tr>
                                 <td>{{$ingredient->tasteless_code}}</td>
                                 <td>{{$ingredient->full_item_description}}</td>
@@ -75,11 +73,11 @@
                             <tr>
                                 <td colspan="4" class="total-cost-label">Total Ingredients Cost</td>
                                 <td class="total-cost peso"></td>
-                            </tr>
+                            </tr> --}}
                     </tbody>
                 </table>
             </div>
-        @endif
+        </div>
     </div>
 </div>
 @endsection
@@ -87,6 +85,54 @@
 @push('bottom')
 <script>
      $(document).ready(function() {
+        const ingredients = {!! json_encode($ingredients) !!};
+        const tbody = $('.ingredient-tbody');
+        const entryCount = [...new Set([...ingredients.map(e => e.ingredient_group)])];
+        for (i of entryCount) {
+            const groupedIngredients = ingredients.filter(e => e.ingredient_group == i);
+            const isSelected = groupedIngredients.filter(e => e.is_selected == 'TRUE');
+            let primary;
+            if (isSelected.length) {
+                primary = isSelected[0];
+            } else {
+                primary = groupedIngredients.filter(e => e.is_primary == 'TRUE')[0];
+            }
+            const tr = $(document.createElement('tr'));
+            for (let i=0; i<5; i++) {
+                const td = $(document.createElement('td'));
+                if (i == 0) td.text(primary.tasteless_code);
+                if (i == 1) td.text(primary.full_item_description);
+                if (i == 2) td.text(primary.qty);
+                if (i == 3) td.text(primary.uom_description);
+                if (i == 4) {
+                    td.text(primary.cost);
+                    td.addClass('cost peso');
+                }
+
+                tr.append(td);
+                console.log(td.html());
+            }
+            $('.ingredient-tbody').append(tr);
+        }
+
+        const totalCostTR = $(document.createElement('tr'));
+        const totalCostLabelTD = $(document.createElement('td'));
+        const totalCostValueTD = $(document.createElement('td'));
+        totalCostLabelTD.attr('colspan', '4');
+        totalCostLabelTD.addClass('total-cost-label');
+        totalCostLabelTD.text('Total Ingredient Cost');
+        totalCostValueTD.addClass('total-cost peso');
+        totalCostTR.append(totalCostLabelTD);
+        totalCostTR.append(totalCostValueTD);
+        $('.ingredient-tbody').append(totalCostTR);
+
+        if (!ingredients.length) {
+            $('.no-ingredient-warning').css('display', '');
+        } else {
+            $('.with-ingredient').css('display', '');
+        }
+
+        console.log(ingredients);
         const costsElems = jQuery.makeArray($('.cost'))
         const totalCostElem = $('.total-cost');
         const totalCost = costsElems.reduce((total, cost) => total + Number($(cost).text().replace(/[^0-9.]/g, '')), 0);
